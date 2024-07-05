494df30ebf1128e22e4556b9d2f768e677cee977
ptdewey <patrickdewey2002@gmail.com>
2024-07-05 07:44:51 -0400

    refactor: changed output file to lua

--- shipwright_build.lua
+++ shipwright_build.lua
@@ -1,8 +1,15 @@
 local colorscheme = require("lush_theme.darkearth")
 local lushwright = require("shipwright.transform.lush")
 
-run(
-    colorscheme,
-    lushwright.to_vimscript,
-    { overwrite, "colors/darkearth.tmp" }
+-- export to lua
+run(colorscheme,
+    lushwright.to_lua,
+    { patchwrite, "colors/darkearth.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
 )
+
+-- export to vimscript
+-- run(
+--     colorscheme,
+--     lushwright.to_vimscript,
+--     { overwrite, "colors/darkearth.tmp" }
+-- )
